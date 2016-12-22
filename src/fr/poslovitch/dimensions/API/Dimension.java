1aaaa26342bb0670279324e3ed089eae3d9f6d89
Poslovitch <poslomc@gmx.fr>
2016-12-22 15:14:09 +0100

    Added day/night duration for Dimension

--- src/fr/poslovitch/dimensions/API/Dimension.java
+++ src/fr/poslovitch/dimensions/API/Dimension.java
@@ -13,18 +13,23 @@ public class Dimension {
 	String name;
 	DimensionGenerator generator;
 	Portal portal;
-	//TODO time (eternal day/night, day/night durations...)
+	int dayDuration, nightDuration;
 	Effects effects;
 	
-	public Dimension(String name, DimensionGenerator generator, Portal portal, Effects effects){
+	// day/night duration in seconds. -1 for eternal.
+	public Dimension(String name, DimensionGenerator generator, Portal portal, Effects effects, int dayDuration, int nightDuration){
 		this.name = name;
 		this.generator = generator;
 		this.portal = portal;
 		this.effects = effects;
+		this.dayDuration = dayDuration;
+		this.nightDuration = nightDuration;
 	}
 	
 	public String getName(){return this.name;}
 	public DimensionGenerator getGenerator(){return this.generator;}
 	public Portal getPortal(){return this.portal;}
 	public Effects getDimensionEffects(){return this.effects;}
+	public int getDayDuration(){return this.dayDuration;}
+	public int getNightDuration(){return this.nightDuration;}
 }
