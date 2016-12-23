bad5768bad56370bc32d891b722a8d473ca404d2
Poslovitch <poslomc@gmx.fr>
2016-12-23 11:06:17 +0100

    Added consumeItem for portal's activator item

--- src/fr/poslovitch/dimensions/API/portals/PortalBuild.java
+++ src/fr/poslovitch/dimensions/API/portals/PortalBuild.java
@@ -10,24 +10,28 @@ import org.bukkit.inventory.ItemStack;
 public class PortalBuild extends Portal{
 
 	PortalPattern pattern;
-	boolean requireIgnition;
-	ItemStack activatorItem = null;
+	boolean requireIgnition, consumeItem;
+	ItemStack activatorItem;
 	
 	public PortalBuild(PortalType type, PortalPattern pattern, boolean requireIgnition) {
 		super(type);
 		this.pattern = pattern;
 		this.requireIgnition = requireIgnition;
+		this.activatorItem = null;
+		this.consumeItem = false;
 	}
 	
-	public PortalBuild(PortalType type, PortalPattern pattern, boolean requireIgnition, ItemStack neededItemToActivate) {
+	public PortalBuild(PortalType type, PortalPattern pattern, boolean requireIgnition, ItemStack neededItemToActivate, boolean consumeItem) {
 		super(type);
 		this.pattern = pattern;
 		this.requireIgnition = requireIgnition;
 		this.activatorItem = neededItemToActivate;
+		this.consumeItem = consumeItem;
 	}
 	
 	public PortalPattern getPattern(){return this.pattern;}
 	public boolean requireIgnition(){return this.requireIgnition;}
 	public boolean needItemToActivate(){return (activatorItem != null);}
+	public boolean consumeItem(){return this.consumeItem;}
 	public ItemStack getActivatorItem(){return this.activatorItem;}
 }
