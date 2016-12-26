43960ff8ff7a920b2320e921fd62e5b5f819a93c
Poslovitch <poslomc@gmx.fr>
2016-12-26 12:00:05 +0100

    Started work on structures

--- src/fr/poslovitch/dimensions/enums/DTree.java
+++ src/fr/poslovitch/dimensions/enums/DTree.java
@@ -1,16 +1,22 @@
 package fr.poslovitch.dimensions.enums;
 
+import fr.poslovitch.dimensions.schematics.Schematic;
+
 /**
  * 
  * @author Poslovitch
  * @version 1.0
  */
 public class DTree {
-		
-	public static final DTree OAK = new DTree();
-	public static final DTree DEAD = new DTree();
 	
-	public DTree(){
-		
+	private Schematic schematic;
+	private StructureGeneration generation;
+	private boolean randomRotation, vinesOnTree;
+	
+	public DTree(Schematic schematic, StructureGeneration generation, boolean randomRotation, boolean vinesOnTree){
+		this.schematic = schematic;
+		this.generation = generation;
+		this.randomRotation = randomRotation;
+		this.vinesOnTree = vinesOnTree;
 	}
 }
